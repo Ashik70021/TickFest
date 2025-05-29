1818b273df2d944200e9b937bd42d639e8dce262
raz421 <maharazbhuiyan3@gmail.com>
2025-05-29 15:48:04 +0600

    cards are added

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,9 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Landing from "../components/Pages/Landing";
 import AdminDashboard from "../layouts/AdminDashboard";
-import AdminDashboardHome from "../components/Pages/AdminDashboard/AdminDashboardHome";
 import AdminManageEvent from "../components/Pages/AdminDashboard/AdminManageEvent";
-import AdminEvents from "../components/Pages/AdminDashboard/AdminEvents";
+
 
 
 export const router = createBrowserRouter([
@@ -25,17 +24,10 @@ export const router = createBrowserRouter([
       element: <AdminDashboard></AdminDashboard>,
       children:[
         {
-          path:"adminhome",
-          element:<AdminDashboardHome></AdminDashboardHome>,
-        },
-        {
           path:"manageevents",
           element:<AdminManageEvent></AdminManageEvent>,
         },
-        {
-          path:"events",
-          element:<AdminEvents></AdminEvents>,
-        },
+
       ]
     }
   ]);
