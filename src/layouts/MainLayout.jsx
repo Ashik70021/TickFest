8e40f1ca5bd4fe6fb50fa5dca1303dc0719b12bf
raz421 <maharazbhuiyan3@gmail.com>
2025-05-29 16:13:53 +0600

    small change in card

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,20 +1,23 @@
-import Navbar from "../components/header/Navbar"
-import Footer from "../components/footer/Footer"
 import { Outlet } from "react-router-dom";
+import CardList from "../components/cards/CardList";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/header/Navbar";
 const MainLayout = () => {
-    return (
-        <div>
-            <div>
-                <Navbar></Navbar>
-            </div>
-            <div className="min-h-screen">
-                <Outlet></Outlet>
-            </div>
-            <div>
-                <Footer></Footer>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div className="min-h-screen">
+        <Outlet>
+          <CardList />
+        </Outlet>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
