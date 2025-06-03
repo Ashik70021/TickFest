72991ebb0b2ec8d78cb08963ca7a4688f4d1cc82
coder-nishu <nishatkh1582@gmail.com>
2025-06-04 01:40:01 +0600

    partner added in landing page

--- src/components/Pages/Landing.jsx
+++ src/components/Pages/Landing.jsx
@@ -1,12 +1,14 @@
 import Banner from "../Banner";
 import CardList from "../cards/CardList";
 import MakeOwnEvent from "../MakeOwnEvent/MakeOwnEvent";
+import Partner from "../Partner";
 const Landing = () => {
     return (
         <div>
             <Banner></Banner>
             <CardList></CardList>
             <MakeOwnEvent></MakeOwnEvent>
+            <Partner></Partner>
         </div>
     );
 };
