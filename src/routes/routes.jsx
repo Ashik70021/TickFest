eddbbc3a59797d4ca501f454e2c4bbaec6a85359
coder-nishu <nishatkh1582@gmail.com>
2025-07-15 01:07:25 +0600

    event card problem fixed before progress

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Landing from "../components/Pages/Landing";
+import Events from "../components/Pages/Events";
 import AdminDashboard from "../layouts/AdminDashboard";
 import AdminDashboardHome from "../components/Pages/AdminDashboard/AdminDashboardHome"; 
 import AdminManageEvent from "../components/Pages/AdminDashboard/AdminManageEvent";
 import AdminEvents from "../components/Pages/AdminDashboard/AdminEvents";
+import AdminUsers from "../components/Pages/AdminDashboard/AdminUsers";
+import AdminTickets from "../components/Pages/AdminDashboard/AdminTickets";
+import AdminReports from "../components/Pages/AdminDashboard/AdminReports";
+import AdminProfile from "../components/Pages/AdminDashboard/AdminProfile";
 import Signin from "../components/Signin";
 import Signup from "../components/Signup";
+import EventDetails from "../components/EventDetails/EventDetails";
+import Checkout from "../components/Checkout/Checkout";
+import ErrorBoundary from "../components/common/ErrorBoundary";
 
 
 export const router = createBrowserRouter([
@@ -19,12 +27,28 @@ export const router = createBrowserRouter([
         element: <Landing />,
       },
       {
+        path: "/events",
+        element: <Events />,
+      },
+      {
         path: "/signin",
         element: <Signin></Signin>
       },
       {
         path: "/signup",
         element: <Signup></Signup>
+      },
+      {
+        path: "/events/:eventId",
+        element: (
+          <ErrorBoundary>
+            <EventDetails />
+          </ErrorBoundary>
+        )
+      },
+      {
+        path: "/checkout",
+        element: <Checkout />
       },
     ],
   },
@@ -44,6 +68,22 @@ export const router = createBrowserRouter([
       {
         path: "events",
         element: <AdminEvents />,
+      },
+      {
+        path: "users",
+        element: <AdminUsers />,
+      },
+      {
+        path: "tickets",
+        element: <AdminTickets />,
+      },
+      {
+        path: "reports",
+        element: <AdminReports />,
+      },
+      {
+        path: "profile",
+        element: <AdminProfile />,
       },
     ],
   },
